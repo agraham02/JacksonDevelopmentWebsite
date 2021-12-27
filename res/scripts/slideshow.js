23a49df946e9d10e8e1c984399e97bd722cb58af
agraham02 <acgraham02@gmail.com>
2021-12-27 13:01:24 -0400

    version 1.0

--- res/scripts/slideshow.js
+++ res/scripts/slideshow.js
@@ -1,25 +1,27 @@
-var banner = document.getElementById("slideshow");
-var slideIndex = 0;
+const banner = document.getElementById("slideshow");
+let slideIndex = 0;
 
-var slides = [
+const slides = [
     {image: "url('./res/images/Buildings/Building1.jpg')", text: "2015 - Anna's Hope"}, 
     {image: "url('./res/images/Buildings/Building2.jpg')", text: "2017 - Magens Junction Phase 1"}, 
     {image: "url('./res/images/Buildings/Building3.jpg')", text: "2021 - Magens Junction Phase 2"} 
 ]
 
 function plusSlides(n) {
-    showSlides(slideIndex += n);
+    slideIndex += n
+    showSlides();
 }
 
 function currentSlide(n) {
-    showSlides(slideIndex = n);
+    slideIndex = n;
+    showSlides();
 }
 
-function showSlides(n) {
-    var dots = document.getElementsByClassName("dot");
+function showSlides() {
+    const dots = document.getElementsByClassName("dot");
 
-    if (n >= slides.length) {slideIndex = 0;}
-    if (n < 0) {slideIndex = slides.length - 1;}
+    if (slideIndex >= slides.length) {slideIndex = 0;}
+    if (slideIndex < 0) {slideIndex = slides.length - 1;}
 
     for (i = 0; i < dots.length; i++) {
         dots[i].className = dots[i].className.replace(" active", "");
@@ -30,4 +32,5 @@ function showSlides(n) {
     dots[slideIndex].className += " active";
 }
 
-showSlides(slideIndex);
+showSlides();
+setInterval(() => plusSlides(1), 5000);
